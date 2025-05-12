cdac7451b5cb728607ff8faeff1b7e38a395ef47
shova1 <shova@co.design>
2025-05-12 17:51:24 +0600

    feat: project initiated and basic layout defined

---
+++ myblogs/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-e%5$l1@-%tsa9&ui6x0nq@81@zl90v20a@7^#$(7*5z!a(fyc#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
