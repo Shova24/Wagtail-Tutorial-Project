d8e65e3535e89746bcb1d8e4fa8afd88bba4ce8f
shova1 <shova@co.design>
2025-05-13 14:49:41 +0600

    feat: add base app with initial models and settings

--- myblogs/settings/base.py
+++ myblogs/settings/base.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
+    "base",
     "blog",
     "home",
     "search",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "wagtail.contrib.settings"
 ]
 
 MIDDLEWARE = [
@@ -75,6 +77,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                 "wagtail.contrib.settings.context_processors.settings",
             ],
         },
     },
